529050c1b84af349601d235aadf578d8499b6f4d
3360260 <72194187+3360260@users.noreply.github.com>
2022-06-09 16:50:46 +0200

    updated read & delete

--- DataBaseAssignment/DataBaseAssignment/SQLDelete.cs
+++ DataBaseAssignment/DataBaseAssignment/SQLDelete.cs
@@ -12,7 +12,41 @@ namespace DataBaseAssignment
 
         public void delete(int entries)
         {
-            //code
+            string ConnectionString = "server=localhost;uid=root;pwd=;database=databaseassignment;";
+
+            MySqlConnection conn;
+            MySqlCommand cmd;
+            MySqlDataAdapter adapter;
+
+            conn = new MySqlConnection();
+
+            int entryValue = entries;
+            conn.ConnectionString = ConnectionString;
+            try
+            {
+                Stopwatch stopw = new Stopwatch();
+                stopw.Start();
+                conn.Open();
+
+                for (int i = 0; i < entryValue; i++)
+                {
+                    cmd = new MySqlCommand();
+                    adapter = new MySqlDataAdapter();
+
+                    cmd.Connection = conn;
+                    cmd.CommandText = "DELETE TOP (" + entryValue + ") FROM user WHERE userID = 1";
+                    cmd.ExecuteNonQuery();
+                }
+                conn.Close();
+
+                stopw.Stop();
+                Console.WriteLine(" Time elapsed: {0} ", stopw.Elapsed);
+
+            }
+            catch (MySql.Data.MySqlClient.MySqlException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
         }
 
     }
--- DataBaseAssignment/DataBaseAssignment/SQLRead.cs
+++ DataBaseAssignment/DataBaseAssignment/SQLRead.cs
@@ -12,6 +12,45 @@ namespace DataBaseAssignment
 
         public void read(int entries)
         {
+            string ConnectionString = "server=localhost;uid=root;pwd=;database=databaseassignment;";
+
+            MySqlConnection conn;
+            MySqlCommand cmd;
+            MySqlDataAdapter adapter;
+
+            conn = new MySqlConnection();
+            cmd = new MySqlCommand();
+            adapter = new MySqlDataAdapter();
+
+            int entryValue = entries;
+            conn.ConnectionString = ConnectionString;
+            try
+            {
+                Stopwatch stopw = new Stopwatch();
+                stopw.Start();
+
+                for (int i = 0; i < 1; i++)
+                {
+                    conn.Open();
+                    cmd.Connection = conn;
+                    cmd.CommandText = "SELECT * FROM user LIMIT " + entryValue;
+                    adapter.SelectCommand = cmd;
+                    using MySqlDataReader rdr = cmd.ExecuteReader();
+
+                    while (rdr.Read())
+                    {
+                        Console.WriteLine("{0} {1} {2}", rdr.GetInt32(0), rdr.GetString(1), rdr.GetString(2));
+                    }
+
+                    stopw.Stop();
+                    Console.WriteLine(" Time elapsed: {0} ", stopw.Elapsed);
+                    conn.Close();
+                }
+            }
+            catch (MySql.Data.MySqlClient.MySqlException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
             //code
         }
         
